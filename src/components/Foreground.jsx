6815f9a91e476545bee8a3df46b52fef74f6787f
Ritik <ritikhjr@gmail.com>
2024-05-07 10:41:52 +0530

    bouncy cards

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Card from "./Card";
 
 function Foreground() {
+  const ref = useRef(null);
+
+  const data = [
+    {
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+      fileSize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+      fileSize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+      fileSize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Upload",
+        tagColor: "blue",
+      },
+    },
+  ];
+  useState();
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
-      <Card />
+    <div
+      ref={ref}
+      className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5"
+    >
+      {data.map((item, index) => (
+        <Card data={item} reference={ref} />
+      ))}
     </div>
   );
 }
